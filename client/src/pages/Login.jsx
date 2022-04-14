58b114e6fb35913789a0ac2c0c93cea46c2758a2
George Kibe <georgekibewambui@gmail.com>
2022-04-14 22:29:24 +0300

    Working on making it mobile responsive

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { mobile } from "../responsive"
 
 const Container = styled.div`
     width:100vw;
@@ -10,9 +11,10 @@ const Container = styled.div`
     justify-content:center;
 `
 const Wrapper = styled.div`
-    padding:20px;
-    width:25%;
+    width:25%;    
+    padding:20px;    
     background-color:white;
+    ${mobile({width:"75%"})}
 `
 const Title = styled.h1`
     font-size:24px;
