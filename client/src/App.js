ae0023a0d9dd7b529ba1662af4dfd0a1b3db5b78
George Kibe <georgekibewambui@gmail.com>
2022-04-15 19:48:44 +0300

    Adding react router dom package and working on routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,26 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import {
+  BrowserRouter as Router,
+  Routes, Redirect,
+  Route
+} from "react-router-dom";
 
 const App = () => {
+  const user =true
   return (
     <div>     
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={<Cart/>} />          
+          <Route path='/product/:id' element={<Product/>} />
+          <Route path='/products/:category' element={<ProductList/>} />
+          <Route path='/login'  element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
