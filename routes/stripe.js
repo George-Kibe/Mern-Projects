a6661050f8bb8885210e91afd4b40d489ceae8f8
George Kibe <georgekibewambui@gmail.com>
2022-04-25 13:51:21 +0300

    testing a stripe transaction verification

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const stripe=require("stripe")(process.env.STRIPE_KEY);
+//const KEY = process.env.STRIPE_KEY
+const KEY ="sk_test_51K7g1nEkdIEftzMHKtN6WuUHQTbncl77hk5FlkqysTpbXbuQOOSOORymb3eBD9FzSkWYDVTiDmiZAPOL7Gfj5OhI00yA9vO2Iw"
+const stripe=require("stripe")(KEY);
 
 router.post("/payment", (req, res) =>{
+    console.log(req.body.tokenId)
+    console.log(req.body.amount)
+    console.log(KEY)
+
     stripe.charges.create(
         {
             source: req.body.tokenId,
@@ -11,8 +17,10 @@ router.post("/payment", (req, res) =>{
         (stripeErr, stripeRes) =>{
             if (stripeErr){
                 res.status(500).json(stripeErr);
+                console.log(stripeErr)
             }else{
                 res.status(200).json(stripeRes);
+                console.log(stripeRes)
             }
         }
     )
