913c8ddfdb47ef33e0e701ae2ec815c2c0cfd7e5
George-Kibe <georgekibewambui@gmail.com>
2024-04-17 17:15:11 +0300

    book crud APIs

--- dockernginx/server/database/db.js
+++ dockernginx/server/database/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectToDB = async () => {
+    console.log("Connecting to MongoDB...");
+    const mongoUri = `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb`
+    console.log("MONGO URI: ", process.env.MONGO_URI);
+    console.log("DB NAME: ", process.env.DB_NAME);
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI, {
+        const connect = await mongoose.connect(mongoUri, {
             dbName: process.env.DB_NAME,  
         });
         console.log(`MongoDB connected: ${connect.connection.host}`);
