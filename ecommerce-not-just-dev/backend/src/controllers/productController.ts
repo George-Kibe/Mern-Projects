c2c73f12c05ac1956d7644004fa37e551983272f
George-Kibe <georgekibewambui@gmail.com>
2024-10-21 00:58:12 +0300

    crud for products done

--- ecommerce-not-just-dev/backend/src/controllers/productController.ts
+++ ecommerce-not-just-dev/backend/src/controllers/productController.ts
@@ -1,37 +1,123 @@
 import { Request, Response } from "express";
+import { db } from "../db";
+import { productsTable } from "../db/productSchema";
+import { eq } from "drizzle-orm";
 
 // list all products
 export async function listProducts(req: Request, res: Response){
-    res.json({
-        products: "Products will be displayed here "
-    })
+    try {
+        const products = await db.select().from(productsTable)
+        res.json({
+            message: "success",
+            products: products,
+            statusCode: 200
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: "Something went wrong"
+        })
+    }
 }
 
 // get product by id
 export async function getProductById(req: Request, res: Response){
-    res.json({
-        product: "Product will be displayed here "
-    })
+    try {
+        const { id } = req.params;
+        const [product] = await db.
+            select()
+            .from(productsTable)
+            .where(eq(productsTable.id, Number(id)))
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+                statuscode: 404
+            })
+        }
+        res.json({
+            message: "success",
+            product: product,
+            statusCode: 200
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: "Something went wrong"
+        })
+    }
 }
 
 // create a new product
 export async function createProduct(req: Request, res: Response){
     console.log(req.body)
-    res.json({
-        product: "Product will be created here "
-    })
+    try {
+        const [product] = await db.
+            insert(productsTable).
+            values(req.body).
+            returning();
+
+        res.status(201).json({
+            message: "Product created successfully",
+            product: product,
+            statusCode: 201
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: "Something went wrong"
+        })
+    }
 }
 
 // update a product
 export async function updateProduct(req: Request, res: Response){
+   try {
+    const id = Number(req.params.id);
+    const updatedFields = req.body;
+    const [updatedProduct] = await db
+        .update(productsTable)
+        .set(updatedFields)
+        .where(eq(productsTable.id, id))
+        .returning();
+
+    if (!updatedProduct) {
+        return res.status(404).json({
+            message: "Product not found",
+            statusCode: 404
+        })
+    }
     res.json({
-        product: "Product will be updated here "
+        message: "Product updated successfully",
+        product: updatedProduct,
+        statusCode: 200
     })
+   } catch (error) {
+        res.status(500).json({
+            error: "Something went wrong"
+        })
+   }
 }
 
 // delete a product
 export async function deleteProduct(req: Request, res: Response){
-    res.json({
-        product: "Product will be deleted here "
-    })
+    try {
+       const [deletedProduct] = await db
+            .delete(productsTable)
+            .where(eq(productsTable.id, Number(req.params.id)))
+            .returning();
+
+        if (!deletedProduct) {
+            return res.status(404).json({
+                message: "Product not found",
+                statusCode: 404
+            })
+        }
+        res.status(200).json({
+            message: "Product deleted successfully",
+            statusCode: 200
+        })        
+    } catch (error) {
+        res.status(500).json({
+            error: "Something went wrong"
+        })
+    }
 }
