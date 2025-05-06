c5f67cbd2c9d5a8b7ea2b1940bbe9caa42c35703
George-Kibe <georgekibewambui@gmail.com>
2025-05-06 19:47:36 +0300

    webhooks

--- lama-blog/backend/index.js
+++ lama-blog/backend/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import connectDB from './lib/connectDB.js';
-// import userRouter from "./routes/user.route.js";
+import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
-// import commentRouter from "./routes/comment.route.js";
+import commentRouter from "./routes/comment.route.js";
 import webhookRouter from "./routes/webhook.route.js";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
 
@@ -28,9 +28,9 @@ app.get('/server-test', (req, res) => {
 });
 
 
-// app.use("/users", userRouter);
+app.use("/users", userRouter);
 app.use("/posts", postRouter);
-// app.use("/comments", commentRouter);
+app.use("/comments", commentRouter);
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
